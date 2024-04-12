bdb87891a40f3cbabb0147b1f5fd907d28e632d4
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-04-12 17:24:52 -0300

    feat(nvim): add vim and theme configs
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- lib/default.nix
+++ lib/default.nix
@@ -45,17 +45,7 @@ in rec {
   mkExtraConfig = 
   ''
   lua << EOF
-    require "core"
-    require("core.utils").load_mappings()
-
-    -- {{{ Auto install lazy.nvim if when needed.
-    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-    if not vim.loop.fs_stat(lazypath) then
-      require("core.bootstrap").lazy(lazypath)
-    end
-    -- ------------------------------------------------------------------------- }}}
-    vim.opt.rtp:prepend(lazypath)
-    require "plugins"
+    require 'fabiosouzadev'.init()
   EOF
   '';
 
