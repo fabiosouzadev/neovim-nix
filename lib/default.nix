73ef29277ddeacb928b9955231725e9cf7486b9a
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-04-15 19:37:10 -0300

    feat: add all plugins
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- lib/default.nix
+++ lib/default.nix
@@ -19,18 +19,75 @@ in rec {
   in [
     # dependecies
     vimPlugins.plenary-nvim
+    vimPlugins.nvim-web-devicons
+
+    # telescope
+    vimPlugins.telescope-nvim
+    vimPlugins.telescope-fzy-native-nvim
     
-    # languages
-    vimPlugins.nvim-lspconfig
+    # treesitter
     vimPlugins.nvim-treesitter.withAllGrammars
+    vimPlugins.nvim-treesitter-textobjects
+    vimPlugins.nvim-treesitter-context
+    vimPlugins.nvim-ts-context-commentstring 
+    vimPlugins.nvim-ts-autotag
+
+    # git
+    vimPlugins.diffview-nvim
+    vimPlugins.neogit
+    vimPlugins.gitsigns-nvim
+    vimPlugins.vim-fugitive
+    
+    # lsp
+    vimPlugins.nvim-lspconfig
     vimPlugins.vim-just
+
+    # cmp
+    vimPlugins.nvim-cmp # <https://github.com/hrsh7th/nvim-cmp>
+    vimPlugins.cmp_luasnip # snippets autocompletion extension for nvim-cmp | <https://github.com/saadparwaiz1/cmp_luasnip/>
+    vimPlugins.lspkind-nvim # vscode-like LSP pictograms | <https://github.com/onsails/lspkind.nvim/>
+    vimPlugins.cmp-nvim-lsp # LSP as completion source | <https://github.com/hrsh7th/cmp-nvim-lsp/>
+    vimPlugins.cmp-nvim-lsp-signature-help # <https://github.com/hrsh7th/cmp-nvim-lsp-signature-help/>
+    vimPlugins.cmp-buffer # current buffer as completion source | <https://github.com/hrsh7th/cmp-buffer/>
+    vimPlugins.cmp-path # file paths as completion source | <https://github.com/hrsh7th/cmp-path/>
+    vimPlugins.cmp-nvim-lua # neovim lua API as completion source | <https://github.com/hrsh7th/cmp-nvim-lua/>
+    vimPlugins.cmp-cmdline # cmp command line suggestions
+    vimPlugins.cmp-cmdline-history # cmp command line history suggestions
+
+    # dap
+    vimPlugins.nvim-dap
+    vimPlugins.nvim-dap-ui
+    vimPlugins.nvim-dap-python
+    vimPlugins.nvim-dap-virtual-text
+    vimPlugins.nvim-jdtls
 
     # theme
     vimPlugins.tokyonight-nvim
 
+    # file manager
+    vimPlugins.oil-nvim
+
+    # bufferline
+    vimPlugins.bufferline-nvim
+
+    # statusline
+    vimPlugins.lualine-nvim
+    vimPlugins.lualine-lsp-progress
+
     # extras
     vimPlugins.vim-tmux-navigator
+    vimPlugins.mini-nvim
+    vimPlugins.comment-nvim
     vimPlugins.todo-comments-nvim
+    vimPlugins.vim-sleuth
+    vimPlugins.conform-nvim
+    vimPlugins.indent-blankline-nvim
+    vimPlugins.vim-be-good
+    vimPlugins.harpoon2
+    vimPlugins.vim-floaterm
+    vimPlugins.fidget-nvim
+    vimPlugins.neodev-nvim
+    vimPlugins.which-key-nvim
 
     # configuration
     fabiosouzadev-new-nvim
@@ -52,8 +109,10 @@ in rec {
     nodePackages."typescript-language-server"
     nodePackages."vscode-langservers-extracted"
     nodePackages."yaml-language-server"
+    nodePackages."prettier"
     pkgs.lua-language-server
     pkgs.nil
+    pkgs.gopls
 
     # formatters
     pkgs.alejandra
