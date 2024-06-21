aee3772c61d0f50b17d2eea74e2daf4207726eff
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-06-21 07:42:01 -0300

    doc: refactor description

--- flake.nix
+++ flake.nix
@@ -1,52 +1,55 @@
 {
-  description = "Neovim configuration for TheAltF4Stream as a plugin";
+  description = "Neovim configuration for fabiovanderleisouza as a plugin";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs @ {
-    self,
-    flake-parts,
-    nixpkgs,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    inputs @ { self
+    , flake-parts
+    , nixpkgs
+    , ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       flake = {
-        lib = import ./lib {inherit inputs;};
+        lib = import ./lib { inherit inputs; };
       };
 
-      systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
+      systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
 
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        ...
-      }: let
-        inherit (pkgs) alejandra just mkShell;
-      in {
-        apps = {
-          nvim = {
-            program = "${config.packages.neovim}/bin/nvim";
-            type = "app";
+      perSystem =
+        { config
+        , self'
+        , inputs'
+        , pkgs
+        , system
+        , ...
+        }:
+        let
+          inherit (pkgs) alejandra just mkShell;
+        in
+        {
+          apps = {
+            nvim = {
+              program = "${config.packages.neovim}/bin/nvim";
+              type = "app";
+            };
+          };
+
+          devShells = {
+            default = mkShell {
+              buildInputs = [ just ];
+            };
+          };
+
+          formatter = alejandra;
+
+          packages = {
+            default = self.lib.mkVimPlugin { inherit system; };
+            neovim = self.lib.mkNeovim { inherit system; };
           };
         };
-
-        devShells = {
-          default = mkShell {
-            buildInputs = [just];
-          };
-        };
-
-        formatter = alejandra;
-
-        packages = {
-          default = self.lib.mkVimPlugin {inherit system;};
-          neovim = self.lib.mkNeovim {inherit system;};
-        };
-      };
     };
 }
+
