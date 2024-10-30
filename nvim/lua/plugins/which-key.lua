8abcf9a9d00dd5347813b578624a1d6678d8d855
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-10-30 19:06:34 -0300

    [fix]: fix which-key

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,23 +1,13 @@
-require('which-key').setup()
-
--- Document existing key chains
-require('which-key').register {
+local wk = require 'which-key'
+wk.add({
   { '<leader>c', group = '[C]ode' },
-  { '<leader>c_', hidden = true },
   { '<leader>d', group = '[D]ocument' },
-  { '<leader>d_', hidden = true },
-  { '<leader>h', group = 'Git [H]unk' },
-  { '<leader>h_', hidden = true },
   { '<leader>r', group = '[R]ename' },
-  { '<leader>r_', hidden = true },
   { '<leader>s', group = '[S]earch' },
-  { '<leader>s_', hidden = true },
-  { '<leader>t', group = '[T]oggle' },
-  { '<leader>t_', hidden = true },
   { '<leader>w', group = '[W]orkspace' },
-  { '<leader>w_', hidden = true },
-}
--- visual mode
-require('which-key').register({
-  { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
-}, { mode = 'v' })
+  { '<leader>t', group = '[T]oggle' },
+  { '<leader>h', group = 'Git [H]unk' },
+}, {
+  mode = { 'v' },
+  { '<leader>h', desc = 'Git [H]unk' },
+})
